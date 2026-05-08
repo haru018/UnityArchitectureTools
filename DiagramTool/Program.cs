7fc53e22cf309bcaa5407d42b3b7408e8789ba7b
haru018 <haru_silverdale@outlook.com>
2026-05-08 17:27:27 +0900

    情報の取捨選択

--- DiagramTool/Program.cs
+++ DiagramTool/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-public class Program
+class Program
 {
     static void Main(string[] args)
     {
@@ -12,7 +12,6 @@ public class Program
 
         var csFiles = Directory.GetFiles(targetDirectory, "*.cs", SearchOption.AllDirectories);
         var syntaxTrees = new List<SyntaxTree>();
-
         foreach (var file in csFiles)
         {
             var code = File.ReadAllText(file);
@@ -23,69 +22,75 @@ public class Program
             .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
             .AddSyntaxTrees(syntaxTrees);
 
-        var namespaceDict = new Dictionary<string, List<INamedTypeSymbol>>();
+        var allTypes = new List<INamedTypeSymbol>();
         var allEdges = new HashSet<string>();
 
         foreach (var tree in syntaxTrees)
         {
             var semanticModel = compilation.GetSemanticModel(tree);
-            var classNodes = tree
+            var typeNodes = tree
                 .GetRoot()
                 .DescendantNodes()
-                .OfType<ClassDeclarationSyntax>();
+                .OfType<TypeDeclarationSyntax>();
 
-            foreach (var node in classNodes)
+            foreach (var node in typeNodes)
             {
-                if (semanticModel.GetDeclaredSymbol(node) is not INamedTypeSymbol classSymbol) continue;
+                if (semanticModel.GetDeclaredSymbol(node) is not INamedTypeSymbol typeSymbol) continue;
 
                 // enumはスキップ
-                if (classSymbol.TypeKind == TypeKind.Enum) continue;
+                if (typeSymbol.TypeKind == TypeKind.Enum) continue;
+
+                // ネストされたクラスはスキップ
+                if (typeSymbol.ContainingType != null) continue;
 
                 // 「具象クラス」はスキップ
-                bool isConcrete = !classSymbol.IsAbstract && classSymbol.TypeKind == TypeKind.Class;
-                bool inheritsFromAbstract = classSymbol.BaseType != null && classSymbol.BaseType.IsAbstract;
+                bool isConcrete = !typeSymbol.IsAbstract && typeSymbol.TypeKind == TypeKind.Class;
+                bool inheritsFromAbstract = typeSymbol.BaseType != null && typeSymbol.BaseType.IsAbstract;
                 if (isConcrete && inheritsFromAbstract) continue;
 
                 // LifetimeScopeはスキップ
-                if (classSymbol.BaseType != null && classSymbol.BaseType.Name == "LifetimeScope") continue;
+                if (typeSymbol.BaseType != null && typeSymbol.BaseType.Name == "LifetimeScope") continue;
 
-                // namespaceの管理
-                string nsName;
-                if (classSymbol.ContainingNamespace.IsGlobalNamespace)
-                    nsName = string.Empty;
-                else
-                    nsName = classSymbol.ContainingNamespace.ToString() ?? string.Empty;
+                allTypes.Add(typeSymbol);
 
-                if (!namespaceDict.ContainsKey(nsName))
-                    namespaceDict[nsName] = [];
-                namespaceDict[nsName].Add(classSymbol);
+                string sourceId = GetSafePumlId(typeSymbol);
 
                 // 継承の抽出
-                if (classSymbol.BaseType != null && classSymbol.BaseType.SpecialType != SpecialType.System_Object)
-                    if (classSymbol.BaseType.Locations.Any(l => l.IsInSource))
-                        allEdges.Add($"{classSymbol.BaseType.Name} <|-- {classSymbol.Name}");
+                if (typeSymbol.BaseType != null && typeSymbol.BaseType.SpecialType != SpecialType.System_Object && typeSymbol.BaseType.Locations.Any(l => l.IsInSource))
+                    allEdges.Add($"{GetSafePumlId(typeSymbol.BaseType)} <|-- {sourceId}");
 
                 //実装の抽出
-                foreach (var iface in classSymbol.Interfaces)
+                foreach (var iface in typeSymbol.Interfaces)
                     if (iface.Locations.Any(l => l.IsInSource))
-                        allEdges.Add($"{iface.Name} <|.. {classSymbol.Name}");
+                        allEdges.Add($"{GetSafePumlId(iface)} <|.. {sourceId}");
 
-                foreach (var member in classSymbol.GetMembers())
+                foreach (var member in typeSymbol.GetMembers())
                 {
                     if (member.IsImplicitlyDeclared) continue;
 
-                    ITypeSymbol targetType;
                     if (member is IFieldSymbol fieldSymbol)
-                        targetType = fieldSymbol.Type;
+                    {
+                        foreach (var fType in ExtractValidTargets(fieldSymbol.Type))
+                            allEdges.Add($"{sourceId} --> {GetSafePumlId(fType)}");
+                    }
                     else if (member is IPropertySymbol propertySymbol)
-                        targetType = propertySymbol.Type;
-                    else
-                        continue;
+                    {
+                        foreach (var pType in ExtractValidTargets(propertySymbol.Type))
+                            allEdges.Add($"{sourceId} --> {GetSafePumlId(pType)}");
+                    }
+                    else if (member is IMethodSymbol methodSymbol)
+                    {
+                        if (methodSymbol.MethodKind != MethodKind.Ordinary && methodSymbol.MethodKind != MethodKind.Constructor) continue;
 
-                    if (targetType.TypeKind == TypeKind.Enum) continue;
+                        // 戻り値の依存
+                        foreach (var rType in ExtractValidTargets(methodSymbol.ReturnType))
+                            allEdges.Add($"{sourceId} ..> {GetSafePumlId(rType)}");
 
-                    if (targetType is INamedTypeSymbol namedTargetType && namedTargetType.Locations.Any(l => l.IsInSource))
-                        allEdges.Add($"{classSymbol.Name} --> {namedTargetType.Name}");
+                        // 引数の依存
+                        foreach (var param in methodSymbol.Parameters)
+                            foreach (var targetType in ExtractValidTargets(param.Type))
+                                allEdges.Add($"{sourceId} ..> {GetSafePumlId(targetType)}");
+                    }
                 }
             }
         }
@@ -100,21 +105,19 @@ public class Program
         sb.AppendLine("set namespaceSeparator .");
         sb.AppendLine("");
 
-        foreach (var kvp in namespaceDict)
+        foreach (var typeSymbol in allTypes)
         {
-            string ns = kvp.Key;
-            if (ns != string.Empty) sb.AppendLine($"namespace {ns} {{");
+            string typeKeyword = typeSymbol.TypeKind == TypeKind.Interface ? "interface" : "class";
+            string pumlId = GetSafePumlId(typeSymbol);
+            string displayName = typeSymbol.Name;
 
-            foreach (var classSymbol in kvp.Value)
-                if (IsInheritingUnityClass(classSymbol, out string unityType))
-                    sb.AppendLine($"    class {classSymbol.Name} {unityType}");
-                else
-                    sb.AppendLine($"    class {classSymbol.Name}");
-
-            if (ns != string.Empty) sb.AppendLine($"}}");
-
-            sb.AppendLine();
+            if (IsInheritingUnityClass(typeSymbol, out string unityType))
+                sb.AppendLine($"{typeKeyword} \"{displayName}\" as {pumlId} {unityType}");
+            else
+                sb.AppendLine($"{typeKeyword} \"{displayName}\" as {pumlId}");
         }
+
+        sb.AppendLine();
 
         foreach (var edge in allEdges)
             sb.AppendLine(edge);
@@ -124,10 +127,57 @@ public class Program
         File.WriteAllText(outputPath, sb.ToString(), Encoding.UTF8);
     }
 
-    static bool IsInheritingUnityClass(INamedTypeSymbol classSymbol, out string unityBaseName)
+    static string GetSafePumlId(INamedTypeSymbol symbol)
+    {
+        string? id = symbol.OriginalDefinition.ToString();
+        if (id == null)
+            return symbol.Name;
+
+        //* ジェネリクス対処
+        int genericIndex = id.IndexOf('<');
+        if (genericIndex >= 0)
+            id = id[..genericIndex];
+
+        return id
+            .Replace("<T>", "")
+            .Replace("<TKey, TValue>", "")
+            .Replace("<", "_")
+            .Replace(">", "_")
+            .Replace(" ", "");
+    }
+
+    // タプルやジェネリクスの中身を再帰的に分解し、有効な依存先をすべて抽出する
+    static IEnumerable<INamedTypeSymbol> ExtractValidTargets(ITypeSymbol target)
+    {
+        if (target is not INamedTypeSymbol named) yield break;
+
+        // タプルの場合は中身を再帰的に展開
+        if (named.IsTupleType)
+        {
+            foreach (var element in named.TupleElements)
+                foreach (var innerTarget in ExtractValidTargets(element.Type))
+                    yield return innerTarget;
+            yield break;
+        }
+
+        // ジェネリクスの場合は <T> の中身を展開する
+        if (named.IsGenericType)
+            foreach (var typeArg in named.TypeArguments)
+                foreach (var innerTarget in ExtractValidTargets(typeArg))
+                    yield return innerTarget;
+
+        if (named.Locations.Any(l => l.IsInSource) &&
+            named.TypeKind != TypeKind.Enum &&
+            named.SpecialType != SpecialType.System_Void &&
+            !named.IsAnonymousType &&
+            named.ContainingType == null)
+            yield return named;
+    }
+
+    static bool IsInheritingUnityClass(INamedTypeSymbol typeSymbol, out string unityBaseName)
     {
         unityBaseName = string.Empty;
-        var currentBase = classSymbol.BaseType;
+        var currentBase = typeSymbol.BaseType;
 
         while (currentBase != null && currentBase.SpecialType != SpecialType.System_Object)
         {
